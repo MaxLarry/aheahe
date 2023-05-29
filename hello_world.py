d8e488609d40bb96016cee91d43bdb0e0ad3cc45
YlarHeartbb <202080010@psu.palawan.ph>
2023-05-29 23:21:56 +0800

    utilize my mysql, and add get_guest function

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,30 @@ from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "db_myhotel"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/guests", methods=["GET"])
+def get_guest():
+    data = data_fetch("""select * from guest""")
+    return make_response(jsonify(data), 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
